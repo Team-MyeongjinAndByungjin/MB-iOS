b1558e74353feb8ebdfdf1e39bccaff177f8310f
Byung jin <bjcho1503@naver.com>
2023-07-04 22:18:25 +0900

    API 추가

---
+++ MB-iOS/MB-iOS/Service/Coupon/API/CouponAPI.swift
@@ -0,0 +1,54 @@
+import Foundation
+import Moya
+
+enum CouponAPI {
+    case getCoupons
+    case saveCoupon(imageURL: String, price: Int, name: String, expiredAt: String)
+    case deleteCoupon(couponID: Int)
+}
+
+extension CouponAPI: TargetType {
+    var baseURL: URL {
+        return URL(string: "http://192.168.1.135:8080/coupons")!
+    }
+    
+    var path: String {
+        switch self {
+        case .deleteCoupon(let couponID):
+            return "\(couponID)"
+        default:
+            return ""
+        }
+    }
+    
+    var method: Moya.Method {
+        switch self {
+        case .getCoupons:
+            return .get
+        case .saveCoupon:
+            return .post
+        case .deleteCoupon:
+            return .delete
+        }
+    }
+    
+    var task: Moya.Task {
+        switch self {
+        case .saveCoupon(let imageURL, let price, let name, let expiredAt):
+            return .requestParameters(
+                parameters: [
+                    "name": name,
+                    "price": price,
+                    "imageUrl": imageURL,
+                    "expiredAt": expiredAt
+                ],
+                encoding: JSONEncoding.default)
+        default:
+            return .requestPlain
+        }
+    }
+    
+    var headers: [String : String]? {
+        return Header.accessToken.header()
+    }
+}
